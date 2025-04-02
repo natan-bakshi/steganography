1343286191bb955ee07e73bdd0c50fa8e191ed71
natib <natib8000@gmai.com>
2025-04-02 10:58:13 +0300

    commit 4.2 lsb blocks

--- rsa_encryption.py
+++ rsa_encryption.py
@@ -1,5 +1,10 @@
 import pyinputplus as pyip
 
+def get_ord(character):
+    char_ord = str(ord(character))
+    if len(char_ord) == 2:
+        char_ord = "0" + char_ord
+    return char_ord
 
 def validate_tuple(text):
     try:
@@ -8,38 +13,40 @@ def validate_tuple(text):
         raise Exception("Input must be a comma-separated list of integers.")
 
 
+BLOCK_SIZE = 2
+
+
 def rsa_encrypt(text):
     key = pyip.inputCustom(validate_tuple, prompt="Enter public key (comma-separated): ")
-    out = ""
-    for char in text:
-        new_char = pow(ord(char), key[0], key[1])
-        out = " ".join([out, str(new_char)])
+    length = len(text)
+    pad = ' '
+    padding_length = BLOCK_SIZE - length % BLOCK_SIZE if length % BLOCK_SIZE != 0 else 0
+    text = text + (pad * padding_length)
+    len_message = len(text)
+    out = []
+    for i in range(0 , len_message - 1, BLOCK_SIZE):
+        block_ord = ""
+        for j in range(i, i + BLOCK_SIZE):
+            char_ord = get_ord(text[j])
+            block_ord += char_ord
+        new_char = str(pow(int(block_ord), key[0], key[1]))
+        out.append(new_char)
+    out = " ".join(out)
     return out
-
 
 def rsa_decrypt(enc_text):
     key = pyip.inputCustom(validate_tuple, prompt="Enter private key (comma-separated): ")
     out = ""
     enc_numbers = list(map(int, enc_text.split()))  # Convert space-separated string to integers
     for num in enc_numbers:
-        new_char = chr(pow(num, key[0], key[1]))  # Decrypt using modular exponentiation
-        out += new_char
+        joined_chars = str(pow(num, key[0], key[1]))
+        for i in range(0, len(joined_chars) - 1, 3):
+            new_char = chr(int(joined_chars[i:i + 3]))
+            out += new_char
     return out
 
-
-
-
-# Example RSA key pairs
 prv_key = (8736751, 69336961)
 pub_key = (1999, 69336961)
 
-# Encrypt and Decrypt Example
-# rsa_encrypted_text = rsa_encrypt("natan bakshi the king", pub_key)
-# print("Encrypted:", rsa_encrypted_text)
-#
-# rsa_decrypted_text = rsa_decrypt(rsa_encrypted_text, prv_key)
-# print("Decrypted:", rsa_decrypted_text)
 
-# ps = 30029, 2309
-# n = 69336961
-# tn = 69304624
+
